81f8af44b55a354d8a4607392b6de4c62bb45dee
Dominik <yt.dominik48n@gmail.com>
2021-03-17 06:44:01 +0100

    Update code style

--- redirect.js
+++ redirect.js
@@ -2,11 +2,17 @@ const redirectMessage = "You will be redirected, please wait a moment...";
 const redirectURL = "https://dominik48n.de";
 
 function redirect() {
-	location.href = this.redirectURL;
+	this.redirectToWebsite(this.redirectURL);
+}
+
+function redirectToWebsite(redirectURL) {
+	location.href = redirectURL;
+
 	console.log(this.redirectMessage);
 	console.log("Forwarding to " + this.redirectURL);
 }
 
 function getRedirectMessage() {
+	console.log("Get the redirect message!");
 	return this.redirectMessage;
 }
